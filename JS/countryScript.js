a5a323e4bafec989df3618ac7bd3839993340c5c
Shadab Khan <shadabkhan7253@gmail.com>
2022-12-15 00:42:57 +0530

    refactor/country covid cases added

--- JS/countryScript.js
+++ JS/countryScript.js
@@ -1,16 +1,19 @@
 // https://data.covid19india.org/data.json
 // alert('Hello');
 
+// https://disease.sh/v3/covid-19/countries
 const parentElement = document.querySelector('.renderData');
 async function fetchData() {
-  const res = await fetch('https://data.covid19india.org/data.json');
+  const res = await fetch('https://api.covid19api.com/summary');
   const data = await res.json();
   render(data);
+  //   console.log(data);
 }
 fetchData();
 
 function render(data) {
-  const statewise = data.statewise;
+  const statewise = data.Countries;
+  console.log(statewise);
 
   statewise.forEach((state) => {
     const tr = document.createElement('tr');
@@ -20,15 +23,18 @@ function render(data) {
     const td4 = document.createElement('td');
     const td5 = document.createElement('td');
     const td6 = document.createElement('td');
+    const td7 = document.createElement('td');
+    const td8 = document.createElement('td');
 
-    td1.innerHTML = state.state;
-    td2.innerHTML = state.confirmed;
-    td3.innerHTML = state.recovered;
-    td4.innerHTML = state.active;
-    td5.innerHTML = state.deaths;
-    td6.innerHTML = state.lastupdatedtime;
+    td1.innerHTML = state.Country;
+    td2.innerHTML = state.TotalConfirmed;
+    td3.innerHTML = state.NewConfirmed;
+    td4.innerHTML = state.TotalDeaths;
+    td5.innerHTML = state.NewDeaths;
+    td6.innerHTML = state.CountryCode;
+    td7.innerHTML = state.Date;
 
-    tr.append(td1, td2, td3, td4, td5, td6);
+    tr.append(td1, td2, td3, td4, td5, td6, td7);
     parentElement.appendChild(tr);
   });
 }
