3a84d44b4e60a79aae5173b6e6f1cbe4313140a3
Shadab Khan <shadabkhan7253@gmail.com>
2022-12-21 02:21:42 +0530

    refactor/home page updated

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,20 @@ var modal = document.querySelector('.myModal');
 var btn = document.querySelector('.modal');
 var modalContent = document.querySelector('.modalContent');
 var span = document.getElementsByClassName('close')[0];
+var coll = document.getElementsByClassName('collapsible');
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  });
+}
 
 loadEvents();
 
